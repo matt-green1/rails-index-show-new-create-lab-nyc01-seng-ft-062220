152a94f09b3050f15c515b36cd3b3f5d3625a4cb
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-17 13:11:07 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class CouponsController < ApplicationController
+    
+    def index
+        @coupons = Coupon.all
+        render :index
+    end
+
+    def new
+        render :new
+    end
+
+    def show
+        @coupon = Coupon.find(params[:id])
+        render :show
+    end
+
+    
+
+    def create
+        #binding.pry
+        @coupon = Coupon.new
+        @coupon.coupon_code = params[:coupon][:coupon_code]
+        @coupon.store = params[:coupon][:store]
+        @coupon.save
+        redirect_to coupon_path(@coupon)
+    end
+
+end
